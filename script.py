da3d103a58be24fd8a32101306751fb2ae1d9ed5
Numa <manu_dewi@hotmail.com>
2023-11-17 17:15:43 +0100

    Added test objects

---
+++ script.py
@@ -0,0 +1,6 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+print(destinations)
+
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+
